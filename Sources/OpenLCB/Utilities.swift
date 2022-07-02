c078e4058aea0fdfa102ae7a1a8d882007b9b902
Randall Wood <297232+rhwood@users.noreply.github.com>
2022-07-02 17:07:48 -0400

    restore accidentally removed import

--- Sources/OpenLCB/Utilities.swift
+++ Sources/OpenLCB/Utilities.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Foundation
+
 struct Utilities {
     
     /// Create a [UInt8] from a number. This does not protect against count being incorrect.
